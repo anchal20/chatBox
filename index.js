9b5b64ce494fe98e5336d111bd9e4f567d2ab033
Aanchal <asachdeva@gainsight.com>
2017-05-26 11:51:33 +0530

    chatbox first commit

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 /**
  * Created by Aanchal on 10/05/17.
  */
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log("user connected");
+    socket.on('disconnect', function () {
+        console.log("user disconnected");
+    });
+    // fetching the chat msg
+    socket.on('chat message', function (msg) {
+        console.log('message is -> ' + msg);
+        // broadcasting the msg to all users
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function () {
+    console.log("Listening to port 3000....");
+});
